b1f6b07298a70ab22bb168c960fca99b01a37ab2
Azat <galimov-azat@bk.ru>
2019-09-18 20:25:57 +0300

    Changes

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,15 @@ public class Main {
         int currentPurchase = 6_700;
         int bonusBound = 1_000;
         int bonusesPerBound;
-            if (totalPurchases>= 150_000) {
-                bonusesPerBound = 100;
-                System.out.println(currentPurchase / bonusBound * bonusesPerBound);
-            }
+        if (totalPurchases >= 150_000) {
+            bonusesPerBound = 100;
+            System.out.println(currentPurchase / bonusBound * bonusesPerBound);
+        } else if (totalPurchases >= 15_001 && totalPurchases <= 150_000) {
+            bonusesPerBound = 70;
+            System.out.println(currentPurchase / bonusBound * bonusesPerBound);
+        } else if (totalPurchases >= 1 && totalPurchases <= 15_000){
+            bonusesPerBound = 50;
+            System.out.println(currentPurchase / bonusBound * bonusesPerBound);
+        }
     }
 }
