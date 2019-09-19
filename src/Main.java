d6ee3e48ad2ca8411d1945742760f3067de9e39e
Azat <galimov-azat@bk.ru>
2019-09-19 23:10:48 +0300

    Correction

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int currentPurchase = 6_700;
         int bonusBound = 1_000;
         int bonusesPerBound;
-        if (totalPurchases >= goldClubCard) {
+        if (totalPurchases >= goldClubCard){
             bonusesPerBound = 100;
             System.out.println(currentPurchase / bonusBound * bonusesPerBound);
         } else if (totalPurchases >= silverClubCard && totalPurchases <= goldClubCard) {
