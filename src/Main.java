51d78f5dc6e8c83cb751de1de7f75ec2eda99343
Azat <galimov-azat@bk.ru>
2019-09-19 23:05:57 +0300

    Доработка

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        int goldClubCard = 150_000;
+        int silverClubCard = 15_001;
+        int blueClubCard = 1;
         int totalPurchases = 173_000;
         int currentPurchase = 6_700;
         int bonusBound = 1_000;
         int bonusesPerBound;
-        if (totalPurchases >= 150_000) {
+        if (totalPurchases >= goldClubCard) {
             bonusesPerBound = 100;
             System.out.println(currentPurchase / bonusBound * bonusesPerBound);
-        } else if (totalPurchases >= 15_001 && totalPurchases <= 150_000) {
+        } else if (totalPurchases >= silverClubCard && totalPurchases <= goldClubCard) {
             bonusesPerBound = 70;
             System.out.println(currentPurchase / bonusBound * bonusesPerBound);
-        } else if (totalPurchases >= 1 && totalPurchases <= 15_000){
+        } else if (totalPurchases >= blueClubCard && totalPurchases < silverClubCard){
             bonusesPerBound = 50;
             System.out.println(currentPurchase / bonusBound * bonusesPerBound);
         }
